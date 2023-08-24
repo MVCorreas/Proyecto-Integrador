3933b8a96bbf12119f9d278a08a0b2616a7b28db
MVCorreas <correasmv@gmail.com>
2023-08-24 12:21:24 -0300

    working

--- rick_and_morty/src/components/SearchBar/SearchBar.jsx
+++ rick_and_morty/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,7 @@
 //import React from "react";
 import { useState } from "react";
 import styles from "./StyledSearch.module.css";
-import {randomCharacter} from '../../App';
+
 
 export default function SearchBar({onSearch, onRandom}) {
 
@@ -13,17 +13,17 @@ export default function SearchBar({onSearch, onRandom}) {
    
    return (
     <div>
-    <div className={styles.StyledSearch}> {/* Usa styles.StyledSearch */}
+    <div className={styles.StyledSearch}> 
       <nav>
         <input
           type='search'
           placeholder='Add an ID'
           onChange={handleChange}
         />
-        <div className={styles.NavButton}> {/* Usa styles.NavButton */}
+        <div className={styles.NavButton}> 
           <button onClick={() => onSearch(id)}>Find Character</button>
         </div>
-        <div className={styles.NavButton}> {/* Usa styles.NavButton */}
+        <div className={styles.NavButton}> 
           <button onClick={onRandom}>Random</button>
         </div>
       </nav>
