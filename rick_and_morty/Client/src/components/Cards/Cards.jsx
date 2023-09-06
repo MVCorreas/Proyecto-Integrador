53a590addd9edfb94274a64ed2872c46b00a33df
MVCorreas <correasmv@gmail.com>
2023-09-06 12:21:42 -0300

    Trying again

--- rick_and_morty/Client/src/components/Cards/Cards.jsx
+++ rick_and_morty/Client/src/components/Cards/Cards.jsx
@@ -1,8 +1,9 @@
 import Card from "../Card/Card";
+import styles from '../Cards/StyledCards.module.css';
 
 export default function Cards({ characters, onClose }) {
   return (
-    <div>
+    <div className={styles.cardsContainer}>
       {characters.map((character) => (
         <Card key={character.id} character={character} onClose={onClose} />
       ))}
