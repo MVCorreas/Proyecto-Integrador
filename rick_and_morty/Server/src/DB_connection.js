d6d84b895082d9b58e51caf5f9ec2e62ea6906fd
MVCorreas <correasmv@gmail.com>
2023-10-02 11:45:56 -0300

    After

--- rick_and_morty/Server/src/DB_connection.js
+++ rick_and_morty/Server/src/DB_connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DEPLOY } = process.env;
 const FavoriteModel = require('../src/models/Favorite');
 const UserModel = require('../src/models/User');
 // const User = require('../src/models/User');
@@ -11,10 +11,16 @@ const UserModel = require('../src/models/User');
 // Recuerda pasarle la información de tu archivo '.env'.
 
 // URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
+// const sequelize = new Sequelize(
+//    // URL
+//    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/rickandmorty`,
+//    { logging: false, native: false }
+// );
+
 const sequelize = new Sequelize(
-   // URL
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/rickandmorty`,
-   { logging: false, native: false }
+  // URL
+  DB_DEPLOY,
+  { logging: false, native: false }
 );
 
 // Para probar la conexion a la BDD:
